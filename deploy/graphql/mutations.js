9966fcb7dfe707efc44086030d9fea1124f94641
Prisha Priyadarshini <150696909+priyadp1@users.noreply.github.com>
2025-09-14 15:31:04 -0400

    Update mutations.js

--- deploy/graphql/mutations.js
+++ deploy/graphql/mutations.js
@@ -1,1 +1,151 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createDriverFeature = /* GraphQL */ `
+  mutation CreateDriverFeature(
+    $input: CreateDriverFeatureInput!
+    $condition: ModelDriverFeatureConditionInput
+  ) {
+    createDriverFeature(input: $input, condition: $condition) {
+      id
+      driverId
+      date
+      miles
+      trips
+      durationH
+      speedMean
+      speedStd
+      harshBrakeRate
+      harshAccelRate
+      sharpTurnRate
+      nightPct
+      schoolZonePct
+      riskScore
+      premium
+      updatedAt
+      createdAt
+      owner
+      __typename
+    }
+  }
+`;
+export const updateDriverFeature = /* GraphQL */ `
+  mutation UpdateDriverFeature(
+    $input: UpdateDriverFeatureInput!
+    $condition: ModelDriverFeatureConditionInput
+  ) {
+    updateDriverFeature(input: $input, condition: $condition) {
+      id
+      driverId
+      date
+      miles
+      trips
+      durationH
+      speedMean
+      speedStd
+      harshBrakeRate
+      harshAccelRate
+      sharpTurnRate
+      nightPct
+      schoolZonePct
+      riskScore
+      premium
+      updatedAt
+      createdAt
+      owner
+      __typename
+    }
+  }
+`;
+export const deleteDriverFeature = /* GraphQL */ `
+  mutation DeleteDriverFeature(
+    $input: DeleteDriverFeatureInput!
+    $condition: ModelDriverFeatureConditionInput
+  ) {
+    deleteDriverFeature(input: $input, condition: $condition) {
+      id
+      driverId
+      date
+      miles
+      trips
+      durationH
+      speedMean
+      speedStd
+      harshBrakeRate
+      harshAccelRate
+      sharpTurnRate
+      nightPct
+      schoolZonePct
+      riskScore
+      premium
+      updatedAt
+      createdAt
+      owner
+      __typename
+    }
+  }
+`;
+export const createTripEvent = /* GraphQL */ `
+  mutation CreateTripEvent(
+    $input: CreateTripEventInput!
+    $condition: ModelTripEventConditionInput
+  ) {
+    createTripEvent(input: $input, condition: $condition) {
+      id
+      driverId
+      ts
+      speed
+      accel
+      brake
+      lat
+      lon
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const updateTripEvent = /* GraphQL */ `
+  mutation UpdateTripEvent(
+    $input: UpdateTripEventInput!
+    $condition: ModelTripEventConditionInput
+  ) {
+    updateTripEvent(input: $input, condition: $condition) {
+      id
+      driverId
+      ts
+      speed
+      accel
+      brake
+      lat
+      lon
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const deleteTripEvent = /* GraphQL */ `
+  mutation DeleteTripEvent(
+    $input: DeleteTripEventInput!
+    $condition: ModelTripEventConditionInput
+  ) {
+    deleteTripEvent(input: $input, condition: $condition) {
+      id
+      driverId
+      ts
+      speed
+      accel
+      brake
+      lat
+      lon
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
 
